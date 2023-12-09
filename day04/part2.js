f2d923096b6aad00834f2f5bb784ccb7b319bf7b
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2023-12-09 21:46:25 +0100

    solve day 04 part 2

--- day04/part2.js
+++ day04/part2.js
@@ -1,78 +1,48 @@
 const fs = require('fs')
 
-const readInput = filename =>
-  fs.readFileSync(filename, 'utf-8').trim().split('\n')
-const lines = readInput('input.txt')
-const height = lines.length
-const width = lines[0].length
+const data = fs
+  .readFileSync('input.txt', 'utf-8')
+  .split('\n\n')
+  .map(x => x.split('\n'))[0]
 
-const initializeEngineSchematic = (height, width) =>
-  Array.from({ length: height }, () => Array.from({ length: width }, () => []))
+const getCards = cardsArr =>
+  cardsArr.map(card => {
+    let [winningNumbers, yourNumbers] = card.split(' | ')
+    winningNumbers = winningNumbers.split(':')[1].trim()
+    return [winningNumbers, yourNumbers]
+  })
 
-const isSymbol = (lines, engineSchematic, i, j, num) => {
-  const item = lines[i]?.[j]
-  const validIndices =
-    0 <= i && i < lines.length && 0 <= j && j < lines[0].length
+const cards = getCards(data)
 
-  if (!validIndices) {
-    return false
-  }
+const cardCounts = []
 
-  if (item === '*') {
-    engineSchematic[i][j].push(num)
-  }
+let totalDecks = 0
+let cardIdx = 0
 
-  return item !== '.' && !isNaN(item)
+for (const card of cards) {
+  const winningNumbers = card[0].split(/\s+/)
+  const yourNumbers = card[1].split(/\s+/)
+
+  let numOfMatches = yourNumbers.filter(num =>
+    winningNumbers.includes(num)
+  ).length
+
+  cardCounts.push({ matches: numOfMatches, copies: 1 })
 }
 
-const processLine = (lines, engineSchematic, i) => {
-  let j = 0
+for (const cardIdx in cardCounts) {
+  const repetitions = cardCounts[+cardIdx].copies
+  const numOfMatches = cardCounts[+cardIdx].matches
 
-  while (j < lines[i].length) {
-    const start = j
-    let num = ''
+  let startIdx = +cardIdx + 1
+  let endIdx = startIdx + numOfMatches
 
-    while (j < lines[i].length && !isNaN(lines[i][j])) {
-      num += lines[i][j]
-      j++
-    }
-
-    if (num !== '') {
-      num = parseInt(num)
-      isSymbol(lines, engineSchematic, i, start - 1, num) ||
-        isSymbol(lines, engineSchematic, i, j, num)
-
-      for (let k = start - 1; k <= j; k++) {
-        isSymbol(lines, engineSchematic, i - 1, k, num) ||
-          isSymbol(lines, engineSchematic, i + 1, k, num)
-      }
-    } else {
-      j++
+  for (let i = 0; i < repetitions; i++) {
+    for (let j = startIdx; j < endIdx; j++) {
+      cardCounts[j].copies++
     }
   }
+  totalDecks += repetitions
 }
 
-const calculateAnswer = (lines, engineSchematic) => {
-  let sum = 0
-
-  for (let i = 0; i < lines.length; i++) {
-    for (let j = 0; j < lines[0].length; j++) {
-      const nums = engineSchematic[i][j]
-
-      if (lines[i][j] === '*' && nums.length === 2) {
-        sum += nums[0] * nums[1]
-      }
-    }
-  }
-
-  return sum
-}
-
-const engineSchematic = initializeEngineSchematic(height, width)
-
-for (let i = 0; i < height; i++) {
-  processLine(lines, engineSchematic, i)
-}
-
-const answer = calculateAnswer(lines, engineSchematic)
-console.log(answer)
+console.log(totalDecks)
